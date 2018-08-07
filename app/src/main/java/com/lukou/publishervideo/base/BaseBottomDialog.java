fa049ad8e7a79251d33620b44c58f9f37661c6b2
chenxutang <chenxutang@lukou.com>
2018-08-07 20:43:53 +0800

    代码优化

--- app/src/main/java/com/lukou/publishervideo/base/BaseBottomDialog.java
+++ app/src/main/java/com/lukou/publishervideo/base/BaseBottomDialog.java
@@ -24,10 +24,8 @@ public abstract class BaseBottomDialog extends BaseDialog {
         Window window = getWindow();
         window.setGravity(Gravity.BOTTOM);
         window.setWindowAnimations(R.style.bottom_menu_animation);
-        WindowManager windowManager = ((Activity) context).getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
         WindowManager.LayoutParams lp = getWindow().getAttributes();
-        lp.width = display.getWidth();
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
         getWindow().setAttributes(lp);
         setCanceledOnTouchOutside(true);
